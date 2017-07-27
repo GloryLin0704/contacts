4e1a7a704e1427787f4d32222f2691fe97ccc93b
W-46ec <30034417+W-46ec@users.noreply.github.com>
2017-07-27 20:38:21 +0800

    put and del

--- routes/contacts/del.js
+++ routes/contacts/del.js
@@ -1,7 +1,25 @@
-module.exports = function(req, res, next){
-    res.json({
-        hello:'del'
+var mongoClient = require('mongodb').MongoClient;
+var DB_CONN_STR = 'mongodb://127.0.0.1:27017/contacts';
+var tb = 'list';
 
-    })
-    
+module.exports = function(req, res, next){
+	mongoClient.connect(DB_CONN_STR,function(err,db){
+		var collection = db.collection(tb);
+		var whereStr = {"uid": req.body.uid};
+		collection.deleteOne(whereStr, function(err,result){
+			if(err || result.result.n === 0){
+				console.log('Error:' + err);
+				res.json({
+					code: 500,
+					msg: 'Error'
+				});
+			} else {
+				res.json({
+					code: 200,
+					msg: 'success'
+				});
+			}
+			db.close();
+		});
+	});
 }
--- routes/contacts/put.js
+++ routes/contacts/put.js
@@ -1,7 +1,33 @@
-module.exports = function(req, res, next){
-    res.json({
-        hello:'put'
+var mongoClient = require('mongodb').MongoClient;
+var DB_CONN_STR = 'mongodb://127.0.0.1:27017/contacts';
+var tb = 'list';
 
-    })
-    
+module.exports = function(req, res, next){
+	mongoClient.connect(DB_CONN_STR,function(err,db){
+		var collection = db.collection(tb);
+		var whereStr = {"uid": req.body.uid};
+		var updateStr = {$set:{
+			name: req.body.name,
+			sex: req.body.sex,
+			schoolId: req.body.schoolId,
+			proclass: req.body.proclass,
+			phone: req.body.phone,
+			birth: req.body.birth
+		}};
+		collection.update(whereStr, updateStr, function(err,result){
+			if(err || result.result.nModified === 0){
+				console.log("Error:" + err);
+				res.json({
+					code: 500,
+					msg: 'Error'
+				});
+			} else {
+				res.json({
+					code: 200,
+					msg: 'success'
+				});
+			}
+			db.close();
+		});
+	});
 }
